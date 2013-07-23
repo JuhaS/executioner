dc56d8f52ef6853e2f12a894a106e58b094abeb0
Juha <suomalainen.juha@gmail.com>
2013-07-24 04:41:59 +0800

    Implemented running commands with Angular. Got rid of JQuery. Improved usability.

--- app.py
+++ app.py
@@ -4,10 +4,13 @@ from twisted.internet import reactor
 from executioner.api import ExecutionerApiHandler
 
 root = File("executioner/www")  # Server folder www as static files
+index = File("executioner/www/index.html")
 root.putChild("api", ExecutionerApiHandler())  # Let Api handler process /api
+root.putChild("results", index)  # Let Api handler process /api
 
 SERVER_PORT = 9000
 
 factory = Site(root)
 reactor.listenTCP(SERVER_PORT, factory)
 reactor.run()
+print "Listening on port: " + str(SERVER_PORT)
--- executioner/www/js/app.js
+++ executioner/www/js/app.js
@@ -1,12 +1,41 @@
-var INVENTORY_URL = "/api/inventory"
-var MODULES_URL = "/api/modules"
+var INVENTORY_URL = "/api/inventory";
+var MODULES_URL = "/api/modules";
+var RUNCOMMAND_URL = "/api/runcommand";
 
-var inventoryController = angular.module("ansible-executor", []);
 
-inventoryController.controller("InventoryCtrl", function InventoryCtrl($scope,  $http){
+
+var app = angular.module("ansible-executor", [], function($routeProvider, $locationProvider) {
+	$locationProvider.html5Mode(true).hashPrefix('!');
+	
+	$routeProvider.when('/', {
+			templateUrl: '/runcmd.html',
+			controller: RunViewCtrl
+	});
+	$routeProvider.when('/results', {
+			templateUrl: '/results.html',
+			controller: ResultsCtrl 
+	});
+	$routeProvider.otherwise({redirectTo: '/'});
+
+});
+
+app.filter('pretty_json', function() {
+    return function(input) {
+    	if (input) {
+    		return JSON.stringify(input, undefined, 2).replace(/\\n/g, "\n") ;
+    	} 
+    	return ""
+    	 
+    }
+});
+
+function ResultsCtrl($scope, $http){
+	$scope.page.header = "Results: "
+};
+
+app.controller("InventoryCtrl", function InventoryCtrl($scope,  $http){
 	$scope.hosts = [];
 	$scope.groups = [];
-	
 	$http.get(INVENTORY_URL).
 	  success(function(inventory, status, headers, config) {
 		  for (var i in inventory) {
@@ -18,13 +47,18 @@ inventoryController.controller("InventoryCtrl", function InventoryCtrl($scope,  
 					console.error("Unknown inventory type for object: " + inventory[i]);
 				}
 		  }
+		  
 	  }).
 	  error(function(data, status, headers, config) {
 		  console.error("Ajax error: " + data);
 	  });
+	
+	$scope.setHost = function(host) {
+		$scope.runnable.hosts = host;
+	}
 });
 
-inventoryController.controller("ModuleCtrl", function InventoryCtrl($scope,  $http){
+app.controller("ModuleCtrl", function ModuleCtrl($scope,  $http){
 	$scope.modules = [];
 	
 	$http.get(MODULES_URL).
@@ -36,4 +70,37 @@ inventoryController.controller("ModuleCtrl", function InventoryCtrl($scope,  $ht
 		error(function(data, status, headers, config) {
 			console.error("Ajax error: " + data);
 		});
+	
+	$scope.setModule = function(module) {
+		$scope.runnable.module = module;
+	}
 });
+
+
+app.controller("WrapperCtrl", function WrapperCtrl($scope,  $http){
+	$scope.page = {};
+	$scope.page.header = "Loading...";
+});
+
+
+function RunViewCtrl($scope, $http, $location){
+	$scope.runnable = {};
+	$scope.page.header = "Run module: "
+	$scope.submitRunnable = function() {
+		$http.get(RUNCOMMAND_URL + 
+				"?host=" + ($scope.runnable.hosts || "") +
+				"&module=" + ($scope.runnable.module || "") +
+				"&attr=" + ($scope.runnable.attr || "")
+			)
+			.success(function(result, status, headers, config) {
+				
+				$scope.page.results = result;
+			})
+			.error(function(data, status, headers, config) {
+				console.error("Ajax error: " + data);
+			});
+		$location.path('/results');
+		return false;
+	}
+};
+
